9a7a59d6f52087dc570131dabc77186b7aae34b2
MichalSurowka <msurowka9@gmail.com>
2022-06-24 20:51:04 +0200

    add playRound function, which plays a single round

--- script.js
+++ script.js
@@ -1,12 +1,37 @@
 function computerPlay() {
     //create array of possibilities to play
-
-    let RPSarr = ['Rock', 'Paper', 'Scissors']
+    let RPSarr = ['rock', 'paper', 'scissors']
 
     //generate random number to choose element from array
     let randomNum = Math.floor(Math.random() * 2);
 
     //return the random pick
-
     return RPSarr[randomNum];
+}
+
+function playRound(playerSelection, computerSelection){
+    //create array of possibilities to play
+    let RPSarr = ['rock', 'paper', 'scissors']
+
+    //changing string to lower case bc of case-insesitivity
+    playerSelection = playerSelection.toLowerCase();
+
+    //checking who wins
+    if(playerSelection === computerSelection){
+        return "There is Draw!"
+    }
+    switch (playerSelection){
+        case 'rock':
+            if(computerSelection === 'paper') return 'You Lose ' + computerSelection + ' beats ' + playerSelection;
+            return 'You win ' + playerSelection+ ' beats ' + computerSelection;
+            break;
+        case 'paper':
+            if(computerSelection === 'scissors') return 'You Lose ' + computerSelection + ' beats ' + playerSelection;
+            return 'You win ' + playerSelection+ ' beats ' + computerSelection;
+            break;
+        case 'scissors':
+            if(computerSelection === 'rock') return 'You Lose ' + computerSelection + ' beats ' + playerSelection;
+            return 'You win ' + playerSelection+ ' beats ' + computerSelection;
+            break;
+    }
 }
