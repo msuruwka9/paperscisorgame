59850f6e84fcbea72baea2ea96a662951afc1177
MichalSurowka <msurowka9@gmail.com>
2022-06-24 17:15:06 +0200

    add ComputerPlay function
    Function return random pick from array of Rock, Scissors, Paper

---
+++ script.js
@@ -0,0 +1,12 @@
+function computerPlay() {
+    //create array of possibilities to play
+
+    let RPSarr = ['Rock', 'Paper', 'Scissors']
+
+    //generate random number to choose element from array
+    let randomNum = Math.floor(Math.random() * 3) + 1;
+
+    //return the random pick
+
+    return RPSarr[randomNum];
+}
